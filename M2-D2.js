3fef161e9d7dacf386b9e42cb6456c8a309d6a30
Giorgio Colliva <95485973+Gramas87@users.noreply.github.com>
2022-01-13 21:42:40 +0100

    almost done

--- M2-D2.js
+++ M2-D2.js
@@ -13,5 +13,28 @@ for (let i = 0; i < genres.length; i++) {
 
  let body = document.getElementsByTagName("body")
  body[0].appendChild(div)
- 
- 
+
+ const deleteRow = () => {
+    const btns = document.querySelectorAll('.btn-del')
+    for (var i = 0; i < btns.length; i++) {
+      btns[i].addEventListener('click', event => {
+        event.target.parentElement.classList.add('fade-out')
+        setTimeout(() => {
+            event.target.parentElement.classList.add('d-none')
+          }, 1600)
+      })
+    }
+  }
+  deleteRow()
+ /* I cant understand why it wont work and it breaks the other button 
+  const addRow = () => {
+    const tBody = (document.querySelector('tbody').innerHTML +=  
+    <tr>
+                    <th scope="row">8</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td class="btn btn-del">delete row</td>
+
+                </tr>)
+  }
+  */
